d50c99d8a0d8e45c65ed5787da84d14f12f29bc4
Henrik trehjørningen <henriktre@gmail.com>
2020-09-14 22:52:31 +0200

    checks if uses battery or batteryLevel

--- index.js
+++ index.js
@@ -1,31 +1,45 @@
-const inquirer = require('inquirer');
-const createEnturService = require('@entur/sdk').default
-const service = createEnturService({ clientName: 'scootiscoot' })
-console.log('find me a scooter app 0.0.0.1 alpha')
-let find = 'find closest scooter'
-let exit = 'exit app'
+const { isBatteryScooter, isBatteryLevelScooter } = require("@entur/sdk");
+const inquirer = require("inquirer");
+const createEnturService = require("@entur/sdk").default;
+const service = createEnturService({ clientName: "scootiscoot" });
+console.log("find me a scooter app 0.0.0.1 alpha");
+let find = "find closest scooter";
+let exit = "exit app";
 async function findScooter() {
   try {
-      const scooters = await service.getScootersByPosition({
-              latitude: 59.908812,
-              longitude: 10.745687,
-              limit: 1 })
-      console.log(scooters)
+    const scooters = await service.getScootersByPosition({
+      latitude: 59.908812,
+      longitude: 10.745687,
+      limit: 1,
+    });
+    let s = scooters[0];
+    let bat;
+    if (s.battery === undefined) s.battery = s.batteryLevel;
+    console.log(
+      `Operator: ${s.operator}
+Battery: ${s.battery}
+Lat: ${s.lat}
+Long: ${s.lon}
+`
+    );
   } catch (error) {
-      console.error(error)
+    console.error(error);
   }
 }
-inquirer.prompt([
-      {
-          type: 'list', message: 'Choose what you want to do', name: 'choice',
-          choices: [ find, exit ]
-      }
+inquirer
+  .prompt([
+    {
+      type: "list",
+      message: "Choose what you want to do",
+      name: "choice",
+      choices: [find, exit],
+    },
   ])
-  .then(({choice}) => {
-    if(choice === find) {
+  .then(({ choice }) => {
+    if (choice === find) {
       findScooter();
     }
   })
-  .catch(error => {
-      console.error(error)
+  .catch((error) => {
+    console.error(error);
   });
